ffe7a5a32d3e8ed4b0557529725195201ad1b07f
stasiazt <stasiazt@gmail.com>
2019-05-23 19:41:06 +0300

    Initial commit

---
+++ src/org/itstep/ApplicationRunner.java
@@ -0,0 +1,14 @@
+package org.itstep;
+
+public class ApplicationRunner {
+
+    public static void main(String[] args) {
+        someMethod("Hi! My name is Nastya!");
+
+    }
+
+    static void someMethod(String msg){
+        System.out.println(msg);
+    }
+}
+
